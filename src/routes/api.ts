b7fe83591970902a1053b64f24c460edd532c929
Pedro Roriz <ph.roriz09@gmail.com>
2022-08-03 21:47:10 -0300

    Aula de api finalizada

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -7,8 +7,14 @@ const router = Router();
 
 router.get('/ping', ApiController.ping);
 router.get('/random', ApiController.random);
-router.get('/nome/:nome', ApiController.nome)
+router.get('/nome/:nome', ApiController.nome);
 
-router.post('/frases', ApiController.createPhrase)
+router.post('/frases', ApiController.createPhrase);
+router.get('/frases', ApiController.listPhrases);
+router.get('/frase/aleatoria', ApiController.randomPhrase);
 
+router.get('/frase/:id', ApiController.getPhrase);
+
+router.put('/frase/:id', ApiController.updatePhrase);
+router.delete('/frase/:id', ApiController.deletePhrase);
 export default router;
